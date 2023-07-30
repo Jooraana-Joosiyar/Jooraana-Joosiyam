b2bbc864a6b1772baaa3139571a1c02eb440627e
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2023-07-30 09:53:07 +0530

    padas added

--- zodiac.py
+++ zodiac.py
@@ -14,7 +14,10 @@ def check_nakshatra(dt):
     xx, ret = swe.calc_ut(jd, swe.MOON, flags)
 
     # Calculate the nakshatra based on the Moon's position
-    nakshatra = math.floor(xx[0] / (360 / 27))  # There are 27 nakshatras, each spanning 360/27 degrees
+    nakshatra = (xx[0] / (360 / 27))  # There are 27 nakshatras, each spanning 360/27 degrees
+    # print(nakshatra)
+    nakshatra = math.floor(nakshatra)
+    nakshatra = nakshatra % 27
     nakshatras = [
         'Ashwini', 'Bharani', 'Krittika', 'Rohini', 'Mrigashira', 'Ardra', 'Punarvasu', 'Pushya', 'Ashlesha',
         'Magha', 'Purva Phalguni', 'Uttara Phalguni', 'Hasta', 'Chitra', 'Swati', 'Vishakha', 'Anuradha',
@@ -22,8 +25,18 @@ def check_nakshatra(dt):
         'Purva Bhadrapada', 'Uttara Bhadrapada', 'Revati'
     ]
     nakshatras_in_tamil = ['அசுவினி', 'பரணி', 'கிருத்திகை', 'ரோகிணி', 'மிருகசிரீஷம்', 'திருவாதிரை', 'புனர்பூசம்', 'பூசம்', 'ஆயில்யம்', 'மகம்', 'பூரம்', 'உத்திரம்', 'ஹஸ்தம்', 'சித்திரை', 'சுவாதி', 'விசாகம்', 'அனுஷம்', 'கேட்டை', 'மூலம்', 'பூராடம்', 'உத்திராடம்', 'திருவோணம்', 'அவிட்டம்', 'சதயம்', 'பூரட்டாதி', 'உத்திரட்டாதி', 'ரேவதி']
-    # return nakshatras[nakshatra]
-    return f'{nakshatras_in_tamil[nakshatra]} / {nakshatras[nakshatra]}'
+
+    pada_degrees = 360 / 108  # Each nakshatra is divided into 4 padas, each spanning 360/108 degrees
+    nakshatra_pada = math.floor(xx[0] / pada_degrees/4)
+    nakshatra_pada = nakshatra_pada % 4
+    # error by 1 pada
+    # b
+    nakshatra_padas = ['1st pada', '2nd pada', '3rd pada', '4th pada']
+    nakshatra_padas_in_tamil = ['1வது பாதம்', '2வது பாதம்', '3வது பாதம்', '4வது பாதம்']
+    
+    # return f'{nakshatras[nakshatra]} - {nakshatra_padas[nakshatra_pada]} / {nakshatras_in_tamil[nakshatra]} - {nakshatra_padas_in_tamil[nakshatra_pada]}'
+    np = nakshatra_pada + 1
+    return f'{nakshatras_in_tamil[nakshatra]} - {np} / {nakshatras[nakshatra]} - {np}'
 
 
 def check_raasi(dt):
@@ -39,6 +52,8 @@ def check_raasi(dt):
 
     # Calculate the raasi based on the Moon's position
     raasi = math.floor(xx[0] / (360 / 12))  # There are 12 raasis, each spanning 360/12 degrees
+    raasi = raasi % 12
+    
     raasis = [
         'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius',
         'Capricorn', 'Aquarius', 'Pisces'
@@ -47,13 +62,26 @@ def check_raasi(dt):
         'மேஷம்', 'ரிஷிபம்', 'மிதுனம்', 'கடகம்', 'சிம்மம்', 'கன்னி', 'துலாம்', 'விருச்சிகம்', 'தனுசு',
         'மகரம்', 'கும்பம்', 'மீனம்'
     ]
+
     return f'{raasis_in_tamil[raasi]} / {raasis[raasi]}'
 
 
 
 # Example usage:
-dt= "12/02/98 06:52:45"
-dt = datetime.datetime.strptime(dt, '%d/%m/%y %H:%M:%S')
-print(check_raasi(dt))
-print(check_nakshatra(dt))
 
+if __name__ == '__main__':
+    from pyperclip import paste
+    dt = paste()
+    dt = '12/02/1998 06:52:45'
+    dt = '8/12/2000 10:55:00'
+    try:
+        dt = datetime.datetime.strptime(dt, '%d/%m/%y %H:%M:%S')
+    except:
+        dt = datetime.datetime.strptime(dt, '%d/%m/%Y %H:%M:%S')
+    print(dt)
+    raasi=(check_raasi(dt))
+    nakshatra=(check_nakshatra(dt))
+    msg = f"இராசி : {raasi}\nநட்சத்திரம் : {nakshatra}"
+    from pyperclip import copy
+    copy(msg)
+    print(msg)
